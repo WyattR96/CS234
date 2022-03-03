c979a9e7f076b8104fb971a2374c1a33402a8c3b
WyattR96 <98620247+WyattR96@users.noreply.github.com>
2022-03-03 16:43:52 -0600

    Add files via upload

--- Part 2/CS234_Project/Appointments.java
+++ Part 2/CS234_Project/Appointments.java
@@ -2,47 +2,47 @@ package CS234_Project;
 
 import java.util.Scanner;
 
-public class Cars {
-
-    public static void menu(){
-
+public class Appointments {
+    public static void menu() {
         Scanner x = new Scanner(System.in);
 
-        while(true){
-
+        while (true) {
             System.out.println("\nWhat would you like to do:");
-            System.out.println("1. Add make");
-            System.out.println("2. Add model");
-            System.out.println("3. List makes");
-            System.out.println("4. List models");
+            System.out.println("1. ");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
             System.out.println("5. Go back");
-            System.out.println("6. Exit");
+            System.out.println("6. Quit");
 
+            //Input Validation
             int input;
-            while(!x.hasNextInt()){
-                System.out.println("invalid input, type a number between 1 and 5");
+            while (!x.hasNextInt()) {
+                System.out.println("invalid input, type a number between 1 and 6");
                 x.next();
             }
+
             input = x.nextInt();
 
             switch(input) {
-                case 1: //TODO: ;
+                case 1: Employees.menu();
                     break;
                 case 2: //TODO: ;
                     break;
-                case 3: //TODO:  ;
+                case 3: //TODO: ;
                     break;
                 case 4: //TODO: ;
                     break;
-                case 5: Main.menu();
+                case 5: //TODO: ;
                     break;
                 case 6: System.exit(0);
                     break;
                 case 7: default: System.out.println(
-                        "You typed "+ input + "\ninvalid input \n Type a number between 1 and 5");
+                        "You typed "+ input + "\ninvalid input \nType a number between 1 and 5");
                     break;
             }
-            System.out.println("\n----------------");
+            System.out.println("----------------");
         }
+
     }
 }

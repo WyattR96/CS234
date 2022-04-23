ae8cb83da741b5b638cc13e497e7a0da23d780fb
WyattR96 <98620247+WyattR96@users.noreply.github.com>
2022-04-22 22:53:21 -0500

    Add files via upload

---
+++ Part 3/CS234_Project_GUI/src/main/java/CS234_Project/Appointments.java
@@ -0,0 +1,220 @@
+package CS234_Project;
+
+import java.util.ArrayList;
+
+/**
+ * This class handles the creation, reading, updating and deleting of objects of class Appointments.
+ */
+class Appointments {
+
+    private static final ArrayList<Appointments> appointmentsList = new ArrayList<>();
+
+    private Clients Client;
+    private Employees Employee;
+    private String Date;
+
+    //Constructor
+    public Appointments(Clients client, Employees employee, String date){
+        this.Client = client;
+        this.Employee = employee;
+        this.Date = date;
+    }
+
+    //Getters and setters
+    public static ArrayList<Appointments> getAppointmentsList(){return appointmentsList;}
+
+    public void setClient(Clients client){ this.Client = client; }
+
+    public Clients getClient(){ return this.Client; }
+
+    public void setEmployee(Employees employee){ this.Employee = employee; }
+
+    public Employees getEmployee(){ return this.Employee; }
+
+    public void setDate(String date){ this.Date = date; }
+
+    public String getDate(){ return this.Date; }
+
+    //Overrides the standard toString method
+    public String toString(){
+        return this.getClient() + " Has an appointment with \n" + this.getEmployee() +" on " + this.getDate() + "\n";
+    }
+
+    /**
+     * This method builds a menu to access everything having to do with the Appointments class.
+     */
+    public static void appointmentsMenu(){
+
+        while(true){
+            System.out.println("\nWhat would you like to do:");
+            System.out.println("1. Create appointment");
+            System.out.println("2. List appointments");
+            System.out.println("3. Update appointment");
+            System.out.println("4. Remove appointment");
+            System.out.println("5. Search appointments");
+            System.out.println("6. Go back");
+            System.out.println("7. Quit");
+
+            int input = Main.intInputValidation();
+
+            switch(input) {
+                case 1: addAppointmentOptions();
+                    break;
+                case 2: listAppointments();
+                    break;
+                case 3: updateAppointment();
+                    break;
+                case 4: removeAppointment();
+                    break;
+                case 5: searchAppointments();
+                    break;
+                case 6: Main.menu();
+                    break;
+                case 7: System.exit(0);
+                    break;
+                case 8: default: System.out.println(
+                        "You typed "+ input + "\ninvalid input");
+                    break;
+            }
+            System.out.println("----------------");
+        }
+    }
+
+    /**
+     * This method gets the client with the appointment, the employee assigned to the appointment and the date of the
+     * appointment, it then calls on addAppointment.
+     */
+    public static void addAppointmentOptions(){
+        System.out.println("Which client needs to make an appointment:");
+        Clients.listClients();
+        int selection = Main.intInputValidation();
+        Clients client = Clients.getClientsList().get(selection);
+
+
+        System.out.println("Which employee is assigned to this appointment:");
+        Employees.listEmployees();
+        int employeeSelection = Main.intInputValidation();
+        Employees employee = Employees.getEmployeeList().get(employeeSelection);
+
+        System.out.println("Type the date of the appointment:");
+        String date = Main.stringInputValidation();
+
+        addAppointment(client, employee, date);
+    }
+
+    /**
+     * This method creates a new object of class employee then adds it to appointmentsList.
+     * @param client The client with the appointment
+     * @param employee The employee assigned to the appointment
+     * @param date The date of the appointment
+     */
+    public static void addAppointment(Clients client,Employees employee, String date){
+        Appointments newAppointment = new Appointments(client,employee,date);
+        getAppointmentsList().add(newAppointment);
+        System.out.println("Appointment added successfully");
+    }
+
+    /**
+     * This method lists all appointments
+     */
+    public static void listAppointments(){
+        int appointmentIndex = 0;
+        for(Appointments appointment: getAppointmentsList()){
+            System.out.println(
+                    "Appointment number: "+ appointmentIndex + "\n" + appointment.getClient() + "\nEmployee: "
+                            + appointment.getEmployee() + "\nDate: " + appointment.getDate() + "\n");
+            appointmentIndex++;
+        }
+    }
+
+    /**
+     * Lists all appointments then gets the users input for the index of the appointment to be updated. It then asks
+     * the user what needs to be updated and changes elements in appointmentsList as needed
+     */
+    public static void updateAppointment(){
+        listAppointments();
+        System.out.println("Write the number of the appointment you would like to update:");
+        int appointmentIndex = Main.arrayInRangeCheck(getAppointmentsList());
+        Appointments appointment = getAppointmentsList().get(appointmentIndex);
+
+        System.out.println("What would you like to update about the appointment:");
+        System.out.println("1. Assigned employee");
+        System.out.println("2. The client");
+        System.out.println("3. The date");
+        System.out.println("4. Cancel");
+        int appointmentEdit = Main.intInputValidation();
+
+        switch(appointmentEdit){
+
+            case 1:
+                System.out.println("Which employee will now be assigned to this appointment:");
+                Employees.listEmployees();
+                int employeeSelection = Main.arrayInRangeCheck(Employees.getEmployeeList());
+
+                Employees newEmployee = Employees.getEmployeeList().get(employeeSelection);
+                appointment.setEmployee(newEmployee);
+                System.out.println("Assigned employee updated successfully");
+                break;
+
+            case 2:
+                System.out.println("Who is the new client:");
+                Clients.listClients();
+                int clientSelection = Main.arrayInRangeCheck(Clients.getClientsList());
+
+                Clients newClient = Clients.getClientsList().get(clientSelection);
+                appointment.setClient(newClient);
+                System.out.println("Client updated successfully");
+                break;
+
+            case 3:
+                System.out.println("What is the new date of the appointment:");
+                String newTime = Main.stringInputValidation();
+
+                appointment.setDate(newTime);
+                System.out.println("Appointment date updated successfully");
+                break;
+            case 4:
+                appointmentsMenu();
+                break;
+
+            case 5: default:
+                    System.out.println("Invalid input, input must be between 1 and 3");
+                break;
+        }
+
+    }
+
+    /**
+     * Gets the index of an appointment then removes it
+     */
+    public static void removeAppointment(){
+        listAppointments();
+        System.out.println("Write the number of the appointment you would like to remove");
+        int appointmentSelection = Main.arrayInRangeCheck(getAppointmentsList());
+
+        getAppointmentsList().remove(appointmentSelection);
+        System.out.println("Appointment removed successfully");
+    }
+
+    /**
+     * This method is used to search appointments. It takes a search input and checks if a client or employee by that
+     * name exist in appointmentsList.
+     */
+    public static void searchAppointments(){
+        System.out.println("Write the Client, Employee or date of the appointment:");
+        String search = Main.stringInputValidation();
+
+        for(int i = 0; i < getAppointmentsList().size();){
+
+            if(getAppointmentsList().get(i).getClient().getClientName().equalsIgnoreCase(search)
+                    || getAppointmentsList().get(i).getDate().equalsIgnoreCase(search)
+                    || getAppointmentsList().get(i).getEmployee().getEmployeeName().equalsIgnoreCase(search))
+            {
+
+                Appointments appointment = getAppointmentsList().get(i);
+                System.out.println(appointment);
+            }
+            i++;
+        }
+    }
+}
---
+++ Part 3/CS234_Project_GUI/src/main/java/CS234_Project/Cars.java
@@ -0,0 +1,226 @@
+package CS234_Project;
+
+import java.util.ArrayList;
+
+/**
+ * This class handles the creation, reading, updating and deleting of objects of the class Cars.
+ */
+class Cars {
+
+    private static final ArrayList<Cars> carsList = new ArrayList<>();
+
+    private String Make;
+    private String Model;
+
+    //Constructor
+    public Cars(String make, String model){
+        this.Make = make;
+        this.Model = model;
+    }
+
+    //Getters and setters
+    public static ArrayList<Cars> getCarsList(){return carsList;}
+
+    public String getMake(){return this.Make;}
+
+    public void setMake(String make){this.Make = make;}
+
+    public String getModel(){return this.Model;}
+
+    public void setModel(String model){this.Model = model;}
+
+    //Overrides the standard toString method
+    public String toString(){return "Make: " + this.getMake() +" | Model: "+ this.getModel();}
+
+    /**
+     * This method builds a menu to access everything having to do with the Cars class.
+     */
+    public static void carsMenu(){
+
+        while(true){
+            System.out.println("\nWhat would you like to do:");
+            System.out.println("1. Add car");
+            System.out.println("2. List cars");
+            System.out.println("3. Update car");
+            System.out.println("4. Remove car");
+            System.out.println("5. Search cars");
+            System.out.println("6. Go back");
+            System.out.println("7. Quit");
+
+            int input = Main.intInputValidation();
+
+            switch(input) {
+                case 1: carType.addCarOptions(); break;
+
+                case 2: listCars(); break;
+
+                case 3: updateCar(); break;
+
+                case 4: removeCar(); break;
+
+                case 5: searchCars(); break;
+
+                case 6: Main.menu(); break;
+
+                case 7: System.exit(0);
+                    break;
+                case 8: default: System.out.println(
+                        "You typed "+ input + "\ninvalid input");
+                    break;
+            }
+            System.out.println("----------------");
+        }
+    }
+
+    /**
+     * Creates new object of class car then adds it to carsList.
+     * @param make The cars make
+     * @param model The cars model
+     */
+    public static void addCar(String make,String model){
+        Cars newCar = new Cars(make,model);
+        getCarsList().add(newCar);
+        System.out.println("Vehicle added successfully");
+    }
+
+    /**
+     * Lists all cars
+     */
+    public static void listCars(){
+        int carIndex = 0;
+        for (Cars car : carsList){
+            System.out.println(carIndex + " " + car);
+            carIndex++;
+        }
+    }
+
+    /**
+     * Gets the index of a car in cars list then updates a given element
+     */
+    public static void updateCar(){
+        listCars();
+        System.out.println("Write the number of the car you would like to update:");
+        int carSelection = Main.arrayInRangeCheck(getCarsList());
+
+        Cars car = getCarsList().get(carSelection);
+
+        System.out.println("What would you like to update about this car:");
+        System.out.println("1. The make");
+        System.out.println("2. The model");
+        System.out.println("3. The type");
+        System.out.println("4. Cancel");
+        int selection = Main.intInputValidation();
+
+        switch (selection){
+            case 1:
+                System.out.println("What is the new make of this vehicle:");
+                String newMake = Main.stringInputValidation();
+
+                car.setMake(newMake);
+                System.out.println("Make updated successfully");
+                break;
+
+            case 2:
+                System.out.println("What is the new model of this vehicle:");
+                String newModel = Main.stringInputValidation();
+
+                car.setModel(newModel);
+                System.out.println("Model updated successfully");
+                break;
+
+            case 3:
+                System.out.println("What is the cars new type");
+                String newType = Main.stringInputValidation();
+
+                String make = car.getMake();
+                String model = car.getModel();
+
+                Cars newCar = new carType(make,model,newType);
+                getCarsList().remove(car);
+                getCarsList().add(carSelection,newCar);
+                System.out.println("Type updated successfully");           
+                break;
+
+            case 4:
+                carsMenu();
+                break;
+
+            case 5: default:
+                System.out.println("Invalid input");
+                break;
+        }
+    }
+
+    /**
+     * Takes a cars make and model then removes it from carsList.
+     */
+    public static void removeCar(){
+        listCars();
+        System.out.println("Write the number of the car you would like to remove:");
+        int carSelection = Main.arrayInRangeCheck(getCarsList());
+
+        getCarsList().remove(carSelection);
+        System.out.println("Vehicle removed successfully");
+    }
+
+    /**
+     * This method is used to search for cars. It takes a maker or model of a car and prints out any instance of that
+     * vehicle in carsList.
+     */
+    public static void searchCars(){
+        System.out.println("Write the make or model of the car you are searching for:");
+        String search = Main.stringInputValidation();
+
+        for(int i = 0; i < getCarsList().size();){
+            if(getCarsList().get(i).getMake().equalsIgnoreCase(search)
+             || getCarsList().get(i).getModel().equalsIgnoreCase(search))
+            {
+                Cars car = getCarsList().get(i);
+                System.out.println(car);
+            }
+            i++;
+        }
+    }
+
+}
+
+class carType extends Cars{
+
+    private String Type;
+
+    public carType(String make, String model, String type){
+        super(make,model);
+        this.Type = type;
+    }
+
+    public void setType(String type){this.Type = type;}
+
+    public String getType(){return this.Type;}
+
+    //Overrides the standard toString method
+    public String toString(){
+        return "Maker: " + this.getMake() + " | Model: " + this.getModel() + " | Type: " + this.getType();
+    }
+
+    public static void addCarOptions(){
+        System.out.println("Write the cars make:");
+        String Make = Main.stringInputValidation();
+
+        System.out.println("Write the cars model:");
+        String model = Main.stringInputValidation();
+
+        System.out.println("What is the cars type:");
+        String type = Main.stringInputValidation();
+
+        Cars carWithType = new carType(Make,model,type);
+        getCarsList().add(carWithType);
+    }
+
+    public static void addCar(String make, String model, String type){
+        Cars newCar = new carType(make,model,type);
+        getCarsList().add(newCar);
+        System.out.println("Vehicle added successfully");
+    }
+
+
+}
